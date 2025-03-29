be83bbf01673bd0a1e53f75c3968229464e98cf9
Steve Hannah <steve@weblite.ca>
2025-03-29 07:40:54 -0700

    fixed main class name

--- projects/javafx-gradle-modular/src/main/java/{{ packagePath }}/{{ mainClass }}.java
+++ projects/javafx-gradle-modular/src/main/java/{{ packagePath }}/{{ mainClass }}.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 import java.util.ResourceBundle;
 
 
-public class App extends Application {
+public class {{ mainClass }} extends Application {
 
 
     public static void main(String[] args) {
